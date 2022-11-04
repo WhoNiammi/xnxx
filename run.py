d9eec9afaf9bb8664b970e5a0dfd81af729e61da
Niammi <100462295+WhoNiammi@users.noreply.github.com>
2022-11-05 06:05:01 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+#----- Gausah Di Ubah Nanti Eror ----#
+#----- Tools 10000% Free ------#
+
+import os
+if __name__ == "__main__":
+   try:
+       os.system("git pull")
+       __import__("xnxx").login()
+   except Exception as e:
+       exit(str(e))
